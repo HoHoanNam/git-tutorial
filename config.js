35e0f1c344f88ce8a8ed1f21756ce547d1d5919d
HoanNam <m8mi0909@gmail.com>
2025-01-06 16:56:22 +0700

    Add more info

--- config.js
+++ config.js
@@ -1,2 +1,6 @@
 console.log('config.js');
+<<<<<<< HEAD
 console.log('version17');
+=======
+console.log('version13');
+>>>>>>> d7228a3 (Version 13)
--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 console.log('index.js');
+<<<<<<< HEAD
 console.log('version17');
+=======
+console.log('version13');
+>>>>>>> d7228a3 (Version 13)
